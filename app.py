293ecbea19da3b492fb4cc9c1b2fb44c1339180b
Maulik Patel <maulikp163@gmail.com>
2019-06-04 22:09:22 +0530

    fix
    Signed-off-by: Maulik Patel <maulikp163@gmail.com>

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import datetime
 import os
 import time
 
+import requests
 import schedule
 
 from proxy import Proxy
@@ -41,12 +42,62 @@ def check():
     print('Done checking Proxy')
     return {"code": 200}
 
+
+def turnOnFetch():
+    api = os.environ['API']
+    appid = os.environ['APP_ID']
+    worker = os.environ['WORKER']
+    url = 'https://api.heroku.com/apps/' + appid + '/formation/' + worker
+    header = {
+        'Accept': 'application/vnd.heroku+json; version=3',
+        'Authorization': 'Bearer ' + api
+    }
+    data = {"quantity": 1, "size": "Free", "type": "worker"}
+    response = requests.request("PATCH", url, data=data, headers=header)
+    print(response.text)
+
+
+def turnOffFetch():
+    api = os.environ['API']
+    appid = os.environ['APP_ID']
+    worker = os.environ['WORKER']
+    url = 'https://api.heroku.com/apps/' + appid + '/formation/' + worker
+    header = {
+        'Accept': 'application/vnd.heroku+json; version=3',
+        'Authorization': 'Bearer ' + api
+    }
+    data = {"quantity": 0, "size": "Free", "type": "worker"}
+    response = requests.request("PATCH", url, data=data, headers=header)
+    print(response.text)
+
+
 if __name__ == '__main__':
 
     schedule.every(2).minutes.do(check)
+    schedule.every().day.at('00:00').do(turnOnFetch)
+    schedule.every().day.at('00:30').do(turnOffFetch)
+    schedule.every().day.at('2:00').do(turnOnFetch)
+    schedule.every().day.at('2:30').do(turnOffFetch)
+    schedule.every().day.at('4:00').do(turnOnFetch)
+    schedule.every().day.at('4:30').do(turnOffFetch)
+    schedule.every().day.at('5:00').do(turnOnFetch)
+    schedule.every().day.at('5:30').do(turnOffFetch)
+    schedule.every().day.at('6:00').do(turnOnFetch)
+    schedule.every().day.at('6:30').do(turnOffFetch)
+    schedule.every().day.at('7:00').do(turnOnFetch)
+    schedule.every().day.at('7:30').do(turnOffFetch)
+    schedule.every().day.at('8:00').do(turnOnFetch)
+    schedule.every().day.at('8:30').do(turnOffFetch)
+    schedule.every().day.at('9:00').do(turnOnFetch)
+    schedule.every().day.at('9:30').do(turnOffFetch)
+    schedule.every().day.at('10:00').do(turnOnFetch)
+    schedule.every().day.at('10:30').do(turnOffFetch)
+    schedule.every().day.at('11:00').do(turnOnFetch)
+    schedule.every().day.at('11:30').do(turnOffFetch)
+
     while True:
         # Checks whether a scheduled task
         # is pending to run or not
         schedule.run_pending()
         time.sleep(10)
-    fetch_and_check()
+    
